beb7f6ad2cb235d57746071181a3e446a10a96c1
varamara <mikahvaarama@gmail.com>
2024-01-15 14:25:20 +0100

    started productservice

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
 import Category_selection from "../../components/CategorySelection/CategorySelection";
+import ProductGrid from "../../components/productGrid/ProductGrid";
 
 const Home: React.FC = () => {
   return (
     <>
     <Hero />
     <Category_selection />
+    <ProductGrid />
     </>
   );
 };
