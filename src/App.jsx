625f0203842fd2f1f730b82de928859cd11e97cb
julia_ssaa <julia_ssaa@hotmail.com>
2024-01-15 14:20:34 +0100

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/navbar";
 import Home from "./views/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./footer.scss";
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="footer">
+      <div className="icons">
+        <img src="src\assets\at.png" />
+        <img src="src\assets\fb.png" />
+        <img src="src\assets\ig.png" /></div>
+      <p>Lorem ipsum dolor</p>
+    </section>
+  );
+};
+
+export default Footer;
