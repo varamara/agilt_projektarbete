b2a4266b6c3ec0bc3637c4ba07260d953f5621ad
varamara <mikahvaarama@gmail.com>
2024-01-15 14:36:58 +0100

    brandsfix

--- src/components/brands/Brands.tsx
+++ src/components/brands/Brands.tsx
@@ -4,20 +4,23 @@ import './brands.scss';
 const Brands: React.FC = () => {
     return (
     <div className='container'>
-        <div className='Image1'>
-            <img src="./H&M.png" alt="" />
+        <div>
+        <img src="src\assets\images\hm.png" />
         </div>
         <div>
-            <img src="./Modström.png" alt="" />
+            <img src="src\assets\images\modstrom.png" alt="" />
         </div>
         <div>
-            <img src="./Modström.png" alt="" />
+            <img src="src\assets\images\OrionLondon.jpg" alt="" />
         </div>
         <div>
-            <img src="./Modström.png" alt="" />
+            <img src="src\assets\images\oddmolly.png" alt="" />
         </div>
         <div>
-            <img src="./Modström.png" alt="" />
+            <img src="src\assets\images\FrankDandy.png" alt="" />
+        </div>
+        <div>
+            <img src="src\assets\images\FrankDandy.png" alt="" />
         </div>
     </div>
     );
