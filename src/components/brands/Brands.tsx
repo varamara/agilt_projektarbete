13a51b935ad2b1224e65e67ca54d01cf290c0bb1
CodeChamp92 <daniel.johansson2@utb.ecutbildning.se>
2024-01-15 13:27:09 +0100

    brands

---
+++ src/components/brands/Brands.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './brands.scss';
+
+const Brands: React.FC = () => {
+    return (
+    <div className='container'>
+        <div className='Image1'>
+            <img src="./H&M.png" alt="" />
+        </div>
+        <div>
+            <img src="./Modström.png" alt="" />
+        </div>
+        <div>
+            <img src="./Modström.png" alt="" />
+        </div>
+        <div>
+            <img src="./Modström.png" alt="" />
+        </div>
+        <div>
+            <img src="./Modström.png" alt="" />
+        </div>
+    </div>
+    );
+};
+
+export default Brands;
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
 import Category_selection from "../../components/CategorySelection/CategorySelection";
+import Brands from "../../components/brands/Brands"
 
 const Home: React.FC = () => {
   return (
     <>
     <Hero />
     <Category_selection />
+    <Brands />
     </>
   );
 };
